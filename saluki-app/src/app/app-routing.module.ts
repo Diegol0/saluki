d2cafa8dfcc19a15d2dbc36028eb6ee09811a630
Diego Lopez <enrique.lopez.barrientos@ericsson.com>
2022-06-06 14:15:19 -0600

    Adding simple authguard implementation Login->Home

--- saluki-app/src/app/app-routing.module.ts
+++ saluki-app/src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './auth.guard';
+import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [{ path: '', component: LoginComponent }];
+const routes: Routes = [
+  { path: '', component: HomeComponent, canActivate: [AuthGuard] },
+  { path: 'login', component: LoginComponent },
+  { path: '**', redirectTo: '' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
