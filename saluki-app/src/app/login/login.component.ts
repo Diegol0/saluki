3b08b02c3387c85d5d2121dcb7fc2d59cf8a4e44
Diego Lopez <enrique.lopez.barrientos@ericsson.com>
2022-06-06 15:28:41 -0600

    Adding form to handle login request

--- saluki-app/src/app/login/login.component.ts
+++ saluki-app/src/app/login/login.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { LoginUserDto } from '../models/service.dto';
+import { SalukiService } from '../services/saluki.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+  loginForm = new FormGroup({
+    username: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required),
+  });
+  constructor(private salukiService: SalukiService) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  login() {
+    let user: LoginUserDto = this.loginForm.getRawValue()!;
+    this.salukiService.login(user).subscribe((data) => {
+      // Todo: Store JWT for future requests
+      console.log(data);
+    });
   }
-
 }
