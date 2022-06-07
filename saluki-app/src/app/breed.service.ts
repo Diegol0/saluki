dadfb7c472372b7db9465a2bbd4c526510a48976
Diego Lopez <enrique.lopez.barrientos@ericsson.com>
2022-06-06 20:44:47 -0600

    Add dog.ceo logic to breed service

--- saluki-app/src/app/breed.service.ts
+++ saluki-app/src/app/breed.service.ts
@@ -1,9 +1,45 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { catchError } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { ErrorHandlerService } from './services/error-handler/error-handler.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class BreedService {
+  constructor(
+    private http: HttpClient,
+    private errorHandlerService: ErrorHandlerService
+  ) {}
 
-  constructor() { }
+  listAll() {
+    return this.http
+      .get<any>(environment.breedURL + 'breeds/list/all')
+      .pipe(
+        catchError((error: HttpErrorResponse) =>
+          this.errorHandlerService.handleError(error)
+        )
+      );
+  }
+
+  listSubBreed(breed: string) {
+    return this.http
+      .get<any>(environment.breedURL + `breeds/${breed}/list`)
+      .pipe(
+        catchError((error: HttpErrorResponse) =>
+          this.errorHandlerService.handleError(error)
+        )
+      );
+  }
+
+  listSubBreedImage(breed: string, subBreed: string) {
+    return this.http
+      .get<any>(environment.breedURL + `breeds/${breed}/${subBreed}/images`)
+      .pipe(
+        catchError((error: HttpErrorResponse) =>
+          this.errorHandlerService.handleError(error)
+        )
+      );
+  }
 }
