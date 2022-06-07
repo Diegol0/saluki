9c74c291521db4568bae3de19d8529130f9d0f1a
Diego Lopez <enrique.lopez.barrientos@ericsson.com>
2022-06-06 18:04:37 -0600

    Adding signup route

--- saluki-app/src/app/app-routing.module.ts
+++ saluki-app/src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './services/auth.guard';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent, canActivate: [AuthGuard] },
   { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
   { path: '**', redirectTo: '' },
 ];
 
