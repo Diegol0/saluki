075b7721620ddc286dc3d1d7186af5faf99ed675
Diego Lopez <enrique.lopez.barrientos@ericsson.com>
2022-06-06 22:18:45 -0600

    Added logic to breed component

--- saluki-app/src/app/breed/breed.component.ts
+++ saluki-app/src/app/breed/breed.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { take } from 'rxjs';
+import { BreedService } from '../breed.service';
 
 @Component({
   selector: 'app-breed',
   templateUrl: './breed.component.html',
-  styleUrls: ['./breed.component.scss']
+  styleUrls: ['./breed.component.scss'],
 })
 export class BreedComponent implements OnInit {
+  breeds: any[] = [];
 
-  constructor() { }
+  constructor(private breedService: BreedService) {}
 
   ngOnInit(): void {
+    this.breeds = [];
+    this.breedService
+      .listAll()
+      .pipe(take(1))
+      .subscribe((data: any) => {
+        for (var key of Object.keys(data.message)) {
+          this.breeds.push({
+            breed: key.charAt(0).toUpperCase() + key.slice(1),
+            subBreeds: data.message[key],
+          });
+        }
+      });
   }
-
 }
